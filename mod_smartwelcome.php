a47e3e8997b0b72c53cc5224130f909a712b43b4
NnNnnNn <on-job@hotmail.com>
2016-02-18 14:47:44 +0700

    Start Module Dev

---
+++ mod_smartwelcome.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Module.mod_smartwelcome
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */ 
+
+defined('_JEXEC') or die;
+
+//include the helper file
+require_once __DIR__.'helper/helper.php';
+
+$show = ModSmartWelcomeHelper::show($params);
+
+if($show){
+	//สั่ง joomla ให้ค้น laypout ที่ mod_smartwelcome และดูค่าที่ผู้ใช้เลือกว่าเป็น layout อะไร
+	require JModuleHelper::getLayoutPath('mod_smartwelcome', $params->get('layout','default'));
+}
